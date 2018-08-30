edbae8bf7b422b5b34d778b99650f8b69beb2bd5
unknown <mendeltem@googlemail.com>
2018-08-30 14:03:02 +0200

    hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,18 @@
 def distance(strand_a, strand_b):
-    pass
+
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Both Arguments have different length")
+
+    distance = 0
+    for i in range(len(strand_a)):
+        if strand_a[i]!= strand_b[i]:
+            distance +=1
+    return distance
+
+
+
+
+
+
+
+distance("GGACG","GGTCG")
