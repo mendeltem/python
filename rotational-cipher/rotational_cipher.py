d2f8665c311e476fc71aae1a37da45fb9035d951
unknown <mendeltem@googlemail.com>
2018-09-02 19:18:00 +0200

    ceaser

---
+++ rotational-cipher/rotational_cipher.py
@@ -0,0 +1,14 @@
+def rotate(text, key):
+    string= ""
+    for i in text:
+        if 48 < ord(i) < 57 or ord(i) == ord(" ") or ord(i) == ord("!") or ord(i)== ord("'") or ord(i)== ord(",") or ord(i)== ord(".") :
+            z = ord(i)
+        else:
+            z = ord(i) + key
+        if  90 < z  and  i.isupper()  or 122 < z and i.islower():
+            z -= 26
+        if i == " " or i=="<" or i==">":
+            string += " "
+        else:
+            string += chr(z)
+    return string
