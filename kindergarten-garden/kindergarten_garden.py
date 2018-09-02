c634f1901958942ba5628c5b725b6bbd26147e0d
unknown <mendeltem@googlemail.com>
2018-09-02 15:32:00 +0200

    Kinder Garten update

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,4 +1,8 @@
+from typing import List, Any, Union
+
+
 class Garden(object):
+
     def __init__(self, string,students = 0):
         dic = {"V":"Violets","R":"Radishes","C":"Clover","G":"Grass"}
         studentlist = ["Alice", "Bob" ,"Charlie" ,"David", "Eve", "Fred", "Ginny"," Harriet","Ileana", "Joseph", "Kincaid","Larry"]
@@ -6,25 +10,16 @@ class Garden(object):
         if students:
             studentlist = students
             studentlist.sort()
-        list1 = []
-        list2 = []
-        list3 = []
-        length = 0
-        #Items appended in a list
-        for z in string:
-            if z != "\n":
-                list1.append(dic[z])
-                length +=1
-        length = length /2
+        list2 = []; list3 = []
 
-        #the list turned in two lists each grom different row
+        length = (len(string) - 1) / 2
         for i in range(int(length)):
-            list2.append(list1[i])
-            list3.append(list1[i+int(length)])
+            list2.append(dic[string[0:int(length)][i]])
+            list3.append(dic[string[int(length)+1:][i]])
         study = {}
-        length = int(length /2)
+
         #the dictionary with students and items get filled
-        for i in range(length):
+        for i in range(int(length /2)):
             study[studentlist[i]] = list2[0+(i*2)]
             study[studentlist[i]] += " "
             study[studentlist[i]] += list2[1+(i*2)]
@@ -33,7 +28,7 @@ class Garden(object):
             study[studentlist[i]] += " "
             study[studentlist[i]] += list3[1+(i*2)]
         self.study = study
-        self.studentlist = studentlist
 
     def plants(self, student):
         return self.study[student].split(" ")
+
--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,9 +1,2 @@
 def sum_of_multiples(limit, multiples):
-    list = []
-    for i in range(limit):
-        for z in multiples:
-             if not (i % z) and i not in list:
-                list.append(i)
-    return sum(list)
-
-
+    return  sum({y for i in multiples for y in range(i, limit, i)})
