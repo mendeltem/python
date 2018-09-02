50d1c8302ee32b55761a1603e9bb8602823dc959
unknown <mendeltem@googlemail.com>
2018-09-02 16:05:59 +0200

    Amstrong Number

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,9 @@
 def is_armstrong(number):
-    pass
+    zahl = 0
+    for i in str(number):
+        zahl+= (int(i) ** len(str(number)))
+    if number == zahl:
+        return True
+    return False
+
+print(is_armstrong(10))
