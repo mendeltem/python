d78a2750d491bf3ad2ff95787a64b58fff397480
Panda <mendeltem@googlemail.com>
2018-09-22 16:58:10 +0200

    update grade school

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,28 +1,57 @@
 from collections import defaultdict
 
 
+
 class School(object):
     def __init__(self):
         self.students = defaultdict(list)
+        self.sorted_list = []
+
+        self.student_class = defaultdict(list)
+        self.student_class = { 1 : "", 2 : "", 3 : "", 4 : ""}
 
     def add_student(self, name, grade):
-        self.students[grade].append(name)
+        self.student_class[grade] += name+";"
 
+        #print(self.student_class)
+        temp = 0
+        string_tmp = ""
+        for i,y in self.student_class.items():
+            temp = y
+            for u in y:
+                if u != ";":
+                    string_tmp += u
+                else:
+                    self.students[i].append(string_tmp)
+                    string_tmp = ""
+                #self.students[i] += u
+
+                #self.students[i].append(u)
+            #print(i,y)
+            temp = 0
+            #print(y)
+
+
+
+
+
+        print("dict: ", self.students)
     def roster(self):
-        studend_list = []
-        slist = []
-        sorted_studends = sorted(self.students)
-
-        for i in sorted_studends:
-            slist.append(sorted(self.students[i]))
-
-        for sublist in slist:
-            for i in sublist:
-                studend_list.append(i)
-
-        return (studend_list)
+        return (self.sorted_list)
 
     def grade(self, grade_number):
         return sorted(self.students[grade_number])
 
+school = School()
+school.add_student("Mendel", 1)
+school.add_student("Bindy", 1)
 
+for name, grade in [
+    ('Peter', 2),
+    ('Anna', 1),
+    ('Barb', 1),
+    ('Zoe', 2),
+    ('Alex', 2),
+    ('Jim', 3),
+    ('Charlie', 1),
+]: school.add_student(name, grade)
